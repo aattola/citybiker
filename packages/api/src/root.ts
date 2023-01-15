34edba5ba81cd5dc75a1af66e868fe532fd72074
JEFFe <leevi@i.jeffe.co>
2023-01-15 18:18:43 +0200

    feat(backend): new api routes

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,10 +1,8 @@
 import { createTRPCRouter } from './trpc'
-import { postRouter } from './router/post'
 import { stationRouter } from './router/station'
 import { journeyRouter } from './router/journey'
 
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   station: stationRouter,
   journey: journeyRouter
 })
--- packages/api/src/router/station.ts
+++ packages/api/src/router/station.ts
@@ -1,14 +1,84 @@
 import { z } from 'zod'
+import { Prisma } from '@citybiker/db'
 import { createTRPCRouter, publicProcedure } from '../trpc'
 
 export const stationRouter = createTRPCRouter({
-  all: publicProcedure.query(async ({ ctx }) => {
-    const stations = await ctx.prisma.station.findMany()
+  getAll: publicProcedure
+    .input(
+      z.object({
+        take: z.number().min(1).max(100).default(10),
+        cursor: z.number().optional().nullable()
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const query: Prisma.StationFindManyArgs = {
+        skip: 1,
+        take: 100,
+        cursor: input.cursor ? { id: input.cursor } : undefined,
+        orderBy: { id: 'desc' }
+      }
 
-    return stations
-  }),
+      const stations = await ctx.prisma.station.findMany(query)
 
-  byId: publicProcedure.input(z.number()).query(({ ctx, input }) => {
-    return ctx.prisma.station.findFirst({ where: { id: input } })
+      return stations
+    }),
+
+  byId: publicProcedure.input(z.number()).query(async ({ ctx, input }) => {
+    // The average distance of a journey starting from the station
+
+    // const avgDepartureStatsPromise = ctx.prisma.journey.aggregate({
+    //   where: {
+    //     departureStationId: input
+    //   },
+    //   _avg: {
+    //     coveredDistance: true,
+    //     duration: true
+    //   }
+    // })
+
+    const stationWithJourneys = await ctx.prisma.station.findFirst({
+      where: { id: input },
+      include: {
+        _count: {
+          select: {
+            startedJourneys: true,
+            endedJourneys: true
+          }
+        }
+      }
+    })
+
+    // Top 5 most popular return stations for journeys starting from the station with prisma
+
+    /**
+     * Top 5 most popular return stations for journeys starting from the station with
+     *
+     * Ottaa lähtöasema (departureStationId) on input
+     * katsoa kaikki journeyt jossa departureStationId = input
+     * Laskea nämä kaikki yhteen groupBy returnStationId
+     * Laskea monta kertaa per yksi tapahtuma
+     * Ja palauttaa tämä sorted by descenfing order
+     *
+     */
+
+    // const topReturnStationsSQL = await ctx.prisma.$queryRaw`
+    //   SELECT
+    //     returnStationId,
+    //     COUNT(returnStationId) AS count
+    //   FROM Journey
+    //   WHERE departureStationId = ${input}
+    //   GROUP BY departureStationId
+    //   ORDER BY count DESC
+    //   LIMIT 5
+    //   `
+
+    // const top5Departure = await ctx.prisma.journey.groupBy({
+    //   where: {
+    //     departureStationId: input
+    //   },
+    //   by: ['returnStationId']
+    // })
+
+    return stationWithJourneys
   })
 })
