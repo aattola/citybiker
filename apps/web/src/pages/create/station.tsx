f1d02413b12fe30c219e7a0a7812940c76f36cf9
JEFFe <leevi@i.jeffe.co>
2023-01-24 20:22:05 +0200

    fix(frontend): station create ui now looks better

--- apps/web/src/pages/create/station.tsx
+++ apps/web/src/pages/create/station.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Input } from '@chakra-ui/react'
+import { Button, Heading, Input, Stack } from '@chakra-ui/react'
 import { stationParser } from '@citybiker/db/src/parsers'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
@@ -35,67 +35,85 @@ const CreateStation = () => {
   })
 
   return (
-    <div>
+    <div className="max-w-2xl m-auto my-4">
       <form onSubmit={onSubmit}>
-        <h1>Create station</h1>
+        <Stack mt="6" spacing="3">
+          <Heading size="md" py={1}>
+            Create station
+          </Heading>
 
-        <Input
-          placeholder="Name"
-          isInvalid={!!errors.finName}
-          {...register('finName', { required: true })}
-        />
-        {errors.finName?.message && <p>{errors.finName.message}</p>}
+          <div>
+            <Input
+              placeholder="Name"
+              isInvalid={!!errors.finName}
+              {...register('finName', { required: true })}
+            />
+            {errors.finName?.message && <p>{errors.finName.message}</p>}
+          </div>
 
-        <Input
-          placeholder="Address"
-          isInvalid={!!errors.finAddress}
-          {...register('finAddress', { required: true })}
-        />
-        {errors.finAddress?.message && <p>{errors.finAddress.message}</p>}
+          <div>
+            <Input
+              placeholder="Address"
+              isInvalid={!!errors.finAddress}
+              {...register('finAddress', { required: true })}
+            />
+            {errors.finAddress?.message && <p>{errors.finAddress.message}</p>}
+          </div>
 
-        <Input
-          placeholder="City"
-          isInvalid={!!errors.finCity}
-          {...register('finCity', { required: true })}
-        />
-        {errors.finCity?.message && <p>{errors.finCity.message}</p>}
+          <div>
+            <Input
+              placeholder="City"
+              isInvalid={!!errors.finCity}
+              {...register('finCity', { required: true })}
+            />
+            {errors.finCity?.message && <p>{errors.finCity.message}</p>}
+          </div>
 
-        <Input
-          placeholder="Operator"
-          isInvalid={!!errors.operator}
-          {...register('operator', { required: true })}
-        />
-        {errors.operator?.message && <p>{errors.operator.message}</p>}
+          <div>
+            <Input
+              placeholder="Operator"
+              isInvalid={!!errors.operator}
+              {...register('operator', { required: true })}
+            />
+            {errors.operator?.message && <p>{errors.operator.message}</p>}
+          </div>
 
-        <Input
-          type="number"
-          placeholder="Capacity"
-          isInvalid={!!errors.capacity}
-          {...register('capacity', { valueAsNumber: true, required: true })}
-        />
-        {errors.capacity?.message && <p>{errors.capacity.message}</p>}
+          <div>
+            <Input
+              type="number"
+              placeholder="Capacity"
+              isInvalid={!!errors.capacity}
+              {...register('capacity', { valueAsNumber: true, required: true })}
+            />
+            {errors.capacity?.message && <p>{errors.capacity.message}</p>}
+          </div>
 
-        <Input
-          type="number"
-          placeholder="X"
-          isInvalid={!!errors.x}
-          {...register('x', { valueAsNumber: true, required: true })}
-        />
-        {errors.x?.message && <p>{errors.x.message}</p>}
+          <div>
+            <Input
+              type="number"
+              placeholder="X"
+              isInvalid={!!errors.x}
+              {...register('x', { valueAsNumber: true, required: true })}
+            />
+            {errors.x?.message && <p>{errors.x.message}</p>}
+          </div>
 
-        <Input
-          type="number"
-          placeholder="Y"
-          isInvalid={!!errors.y}
-          {...register('y', { valueAsNumber: true, required: true })}
-        />
-        {errors.y?.message && <p>{errors.y.message}</p>}
+          <div>
+            <Input
+              type="number"
+              placeholder="Y"
+              isInvalid={!!errors.y}
+              {...register('y', { valueAsNumber: true, required: true })}
+            />
+            {errors.y?.message && <p>{errors.y.message}</p>}
+          </div>
 
-        <Button isLoading={mutation.isLoading} type="submit">
-          Create
-        </Button>
+          <Button isLoading={mutation.isLoading} type="submit">
+            Create
+          </Button>
 
-        {mutation.isError && <p>{mutation.error.message}</p>}
+          {mutation.isError && <p>Error: {mutation.error.message}</p>}
+        </Stack>
       </form>
     </div>
   )
