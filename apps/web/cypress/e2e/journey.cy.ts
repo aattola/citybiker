62632ae1401afe358f21d8ee800914df1bf58cfb
JEFFe <leevi@i.jeffe.co>
2023-01-29 01:43:09 +0200

    feat(frontend): e2e testing with cypress

---
+++ apps/web/cypress/e2e/journey.cy.ts
@@ -0,0 +1,28 @@
+describe('Journeys list', () => {
+  beforeEach(function () {
+    cy.visit('http://localhost:3000/journeys')
+  })
+
+  it('loads the correct data for journeys', () => {
+    cy.contains('Departure station')
+    cy.contains('Return station')
+    cy.contains('Distance travelled')
+    cy.contains('Duration of the ride')
+
+    cy.contains('Lastenlehto')
+    cy.contains('Itämerentori')
+    cy.contains('Vanha Kauppahalli')
+    cy.contains('0.7')
+    cy.contains('3m 56s')
+  })
+
+  it('search works correctly', () => {
+    cy.get('[placeholder="Search for journeys"]').type('Puistolantori')
+
+    cy.contains('Puistolantori')
+    cy.contains('0.9')
+    cy.contains('6m 11s')
+  })
+
+  // TODO: add more when filtering is implemented
+})
